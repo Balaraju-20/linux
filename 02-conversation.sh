f54c86039578253150fac5f2b5523016adc78f2b
“balaraju” <devopsbalaraju@gmail.com>
2025-03-20 12:15:42 +0530

    conversation

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Ramesh:: Hi Suresh, How are you?"
-echo "Suresh:: Hello Ramesh. I am fine. How are you doing?"
-echo "Ramesh:: I am doing good Suresh. What's going on?"
-echo "Suresh:: I started learning Shell Script Ramesh"
+echo "balaraju:: Hi raaju, How are you?"
+echo "raaju:: Hello balaraju. I am fine. How are you doing?"
+echo "balaraju:: I am doing good raaju. What's going on?"
+echo "raaju:: I started learning Shell Script balaraju"
