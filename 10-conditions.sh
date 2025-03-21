5342d359902dff82fc484574c12a01d38e3a3534
“balaraju” <devopsbalaraju@gmail.com>
2025-03-21 18:19:51 +0530

    conditions

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
-
 NUMBER=$1
 if [ $NUMBER -gt 20 ] #gt, lt, eq, -ne, -ge, -le
 then
-    echo -e $G "Given number: $NUMBER is greater than $N 20"
+    echo  "Given number: $NUMBER is greater than  20"
 else
-    echo -e $R "Given number: $NUMBER is less than $N 20"
+    echo  "Given number: $NUMBER is less than  20"
 fi
